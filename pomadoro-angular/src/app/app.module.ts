e9099f365d4038637fd2b0175a70c003ef8ae5fe
Eugene Izotov <izotov1995@gmail.com>
2023-11-02 18:45:55 +0200

    Adjust app component

--- pomadoro-angular/src/app/app.module.ts
+++ pomadoro-angular/src/app/app.module.ts
@@ -3,36 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './components/card/card.component';
-import { HeaderComponent } from './components/header/header.component';
-import { InputNumberComponent } from './components/input-number/input-number.component';
-import { InputRadioComponent } from './components/input-radio/input-radio.component';
-import { InputTextComponent } from './components/input-text/input-text.component';
-import { EditTaskModalComponent } from './components/edit-task-modal/edit-task-modal.component';
-import { TaskComponent } from './components/task/task.component';
-import { ButtonIconComponent } from './components/button-icon/button-icon.component';
-import { ButtonTextComponent } from './components/button-text/button-text.component';
-import { ButtonComponent } from './components/button/button.component';
-import { IconComponent } from './components/icon/icon.component';
+import { LayoutComponent } from './base/components/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
-    HeaderComponent,
-    InputNumberComponent,
-    InputRadioComponent,
-    InputTextComponent,
-    EditTaskModalComponent,
-    TaskComponent,
-    ButtonIconComponent,
-    ButtonTextComponent,
-    ButtonComponent,
-    IconComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    LayoutComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
