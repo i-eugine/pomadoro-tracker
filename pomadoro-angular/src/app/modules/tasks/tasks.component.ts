ef2426c993f97006e026c3983ecc09415a892c7d
Eugene Izotov <izotov1995@gmail.com>
2023-11-02 18:47:03 +0200

    Add tasks page

--- pomadoro-angular/src/app/modules/tasks/tasks.component.ts
+++ pomadoro-angular/src/app/modules/tasks/tasks.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { IconName } from 'src/app/base/components/icon/icon.component';
+import { ITask } from 'src/app/models/task.interface';
 
 @Component({
   selector: 'pom-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TasksComponent {
 
+  readonly addIcon = IconName.add;
+  
+  tasks: ITask[] = [
+    {
+      category: "hobby",
+      priority: "urgent",
+      date: "TODAY",
+      caption: "Lorem ipsum dolor sit.",
+      note: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, incidunt.",
+      timer: 3,
+    },
+    {
+      category: "sport",
+      priority: "hight",
+      date: "TODAY",
+      caption: "Lorem ipsum dolor sit.",
+      note: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, incidunt.",
+      timer: 3,
+    },
+    {
+      category: "work",
+      priority: "hight",
+      date: "TODAY",
+      caption: "Lorem ipsum dolor sit.",
+      note: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, incidunt.",
+      timer: 3,
+    },
+    {
+      category: "education",
+      priority: "middle",
+      date: "TODAY",
+      caption: "Lorem ipsum dolor sit.",
+      note: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, incidunt.",
+      timer: 3,
+    },
+    {
+      category: "other",
+      priority: "low",
+      date: "TODAY",
+      caption: "Lorem ipsum dolor sit.",
+      note: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, incidunt.",
+      timer: 3,
+    },
+  ];
 }
--- pomadoro-angular/src/app/modules/tasks/tasks.module.ts
+++ pomadoro-angular/src/app/modules/tasks/tasks.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksComponent } from './tasks.component';
-
+import { RouterModule } from '@angular/router';
+import { ButtonIconComponent } from 'src/app/base/components/button-icon/button-icon.component';
+import { TaskComponent } from 'src/app/base/components/task/task.component';
 
 
 @NgModule({
@@ -9,7 +11,12 @@ import { TasksComponent } from './tasks.component';
     TasksComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+
+    ButtonIconComponent,
+    TaskComponent,
+
+    RouterModule.forChild([{ path: '', component: TasksComponent}])
   ]
 })
 export class TasksModule { }
