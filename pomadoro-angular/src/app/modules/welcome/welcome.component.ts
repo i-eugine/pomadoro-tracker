87c0f8e06969e44b780d2e5975fc4ae8761d4d66
Eugene Izotov <izotov1995@gmail.com>
2023-11-02 18:46:52 +0200

    Add welcome page

--- pomadoro-angular/src/app/modules/welcome/welcome.component.ts
+++ pomadoro-angular/src/app/modules/welcome/welcome.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'pom-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class WelcomeComponent {
-
 }
--- pomadoro-angular/src/app/modules/welcome/welcome.module.ts
+++ pomadoro-angular/src/app/modules/welcome/welcome.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeComponent } from './welcome.component';
-
-
+import { CardComponent } from 'src/app/base/components/card/card.component';
+import { ButtonComponent } from 'src/app/base/components/button/button.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     WelcomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+
+
+    CardComponent,
+    ButtonComponent,
   ]
 })
 export class WelcomeModule { }
