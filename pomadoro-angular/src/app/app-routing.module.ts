872949c0724332c51134520344b022c4fba0e0c4
Eugene Izotov <izotov1995@gmail.com>
2023-11-02 18:45:17 +0200

    Add routing

--- pomadoro-angular/src/app/app-routing.module.ts
+++ pomadoro-angular/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeModule } from './modules/welcome/welcome.module';
+import { WelcomeComponent } from './modules/welcome/welcome.component';
+import { LayoutComponent } from './base/components/layout/layout.component';
 
-const routes: Routes = [];
+export enum ROUTES {
+  welcome = 'welcome',
+  tasks = 'tasks',
+  settings = 'settings'
+}
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: `${ROUTES.welcome}`},
+  { path: `${ROUTES.welcome}`, component: WelcomeComponent},
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: `${ROUTES.tasks}`,
+        loadChildren: async () => (await import('./modules/tasks/tasks.module')).TasksModule
+      },
+      {
+        path: `${ROUTES.settings}`,
+        loadChildren: async () => (await import('./modules/settings/settings.module')).SettingsModule
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
