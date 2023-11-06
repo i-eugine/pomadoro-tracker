3a724d6aa8f6cb80c6d6e93297be5e3278426752
Eugene Izotov <izotov1995@gmail.com>
2023-11-06 22:00:11 +0200

    Add input text

--- pomadoro-angular/src/app/base/components/input-text/input-text.component.ts
+++ pomadoro-angular/src/app/base/components/input-text/input-text.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
+
+type InputTextFunc = (_: string) => void;
+
 
 @Component({
-  selector: 'app-input-text',
+  standalone: true,
+  selector: 'pom-input-text',
   templateUrl: './input-text.component.html',
-  styleUrls: ['./input-text.component.scss']
+  styleUrls: ['./input-text.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class InputTextComponent {
+export class InputTextComponent implements ControlValueAccessor {
+  @Input() id = '';
+  @Input() placeholder = '';
+  @Input() title = '';
 
+  onChange: InputTextFunc = () => {};
+  onTouch:  InputTextFunc = () => {};
+
+  _value = '0';
+  @Input() set value(val: string) { 
+    this._value = val;
+
+    this.onChange(val);
+    this.onTouch(val);
+  }
+
+  writeValue(value: string) { this.value = value }
+
+  registerOnChange(fn:  InputTextFunc) { this.onChange = fn }
+  registerOnTouched(fn:  InputTextFunc) { this.onTouch = fn } 
 }
